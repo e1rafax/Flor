5f876be2ed8deb4af3160f86ee155ea8b0e85943
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 13:01:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('mensaje').style.display = 'block';
+});
 
-const sunflower = document.getElementById("sunflower");
-const stem = document.createElement("div");
-const petal = document.createElement("div");
-const center = document.createElement("div");
-const message = document.getElementById("message");
-
-stem.classList.add("stem");
-petal.classList.
