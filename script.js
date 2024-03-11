fde47bee8f60146d3a7b3508b2549edde92c1eb4
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 13:42:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('mensaje').style.display = 'block';
   createPetals();
+  setTimeout(function() {
+    document.getElementById('mensaje').style.opacity = '1';
+  }, 5000); // Show message after 5 seconds
 });
+
 function createPetals() {
-  const petalsContainer = document.getElementById('petals');
-  for (let i = 0; i < 12; i++) {
+  const petalContainer = document.getElementById('petal-container');
+  for (let i = 0; i < 10; i++) {
     const petal = document.createElement('div');
     petal.classList.add('petal');
-    petal.style.transform = `rotate(${i * 30}deg)`;
-    petalsContainer.appendChild(petal);
+    petal.style.left = `${i * 10}px`;
+    petalContainer.appendChild(petal);
   }
 }
+
 
 
 
